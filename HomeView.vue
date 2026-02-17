1e332bcf9afee966ebb52f26938146ffddaeb131
leathware <leathware@gmail.com>
2026-02-17 13:06:14 -0500

    Add files via upload

---
+++ HomeView.vue
@@ -0,0 +1,403 @@
+<template>
+    <div class="wrapper" id="wrapper" style="position: relative;">
+        <div class="check-box" id="check-box" :style="selectedFont ? { fontFamily: selectedFont } : {}">
+            <div style="position: relative;" id="check-box-print">
+                <div class="account-holder-name" style="position: absolute; top: 40px; left: 60px">{{check.accountHolderName}}</div>
+                <div v-if="check.accountHolderNameSecondary" class="account-holder-name" style="position: absolute; top: 65px; left: 60px">{{check.accountHolderNameSecondary}}</div>
+                <div class="account-holder-address" style="position: absolute; top: 70px; left: 60px">
+                    {{check.accountHolderAddress}}<br>
+                    {{check.accountHolderCity}}, {{check.accountHolderState}} {{check.accountHolderZip}}
+                </div>
+                <div class="check-number-human" style="position: absolute; top: 40px; left: 1060px">{{check.checkNumber}}</div>
+                <div v-if="!printBlank" class="date-data" style="position: absolute; top: 80px; left: 850px">{{check.date}}</div>
+                <div class="date" style="position: absolute; top: 90px; left: 760px">Date: _____________________ </div>
+                <div class="amount-box" style="position: absolute; top: 175px; left: 950px">
+
+                </div>
+                <div v-if="!printBlank" class="amount-data" style="position: absolute; top: 182px; left: 970px">{{formatMoney(check.amount)}}</div>
+                <div v-if="!printBlank" class="pay-to-data" style="position: absolute; top: 180px; left: 180px">{{check.payTo}}</div>
+                <div class="pay-to" style="position: absolute; top: 170px; left: 60px">
+                    Pay to the <br>Order of <span class="payto-line"></span>
+                </div>
+                <div v-if="!printBlank" class="amount-line-data" ref="line" style="position: absolute; top: 240px; left: 100px">
+                    ***
+                    {{toWords(check.amount)}} 
+                    ***
+                </div>
+                <div class="amount-line" style="position: absolute; top: 250px; left: 60px">
+                    <span class="dollar-line"></span>
+                </div>
+                <div class="bank-name" style="position: absolute; top: 300px; left: 60px">{{check.bankName}}</div>
+                <div v-if="!printBlank" class="memo-data" style="position: absolute; top: 367px; left: 120px">{{check.memo}}</div>
+                <div class="memo" style="position: absolute; top: 390px; left: 60px">
+                    Memo: ____________________________________
+                </div>
+                <div v-if="!printBlank" class="signature-data" style="position: absolute; top: 366px; left: 770px">{{check.signature}}</div>
+                <div class="signature" style="position: absolute; top: 390px; left: 750px">
+                    _________________________________________________
+                </div>
+                <div class="banking" style="position: absolute; top: 420px; left: 80px">
+                    <div class="routing" style="display: inline;">
+                        a{{check.routingNumber}}a
+                    </div>
+                    <div class="bank-account" style="display: inline;">{{check.bankAccountNumber}}c</div>
+                    <div class="check-number" style="display: inline; margin-left:20px">{{check.checkNumber}}</div>
+                </div>
+            </div>
+        </div>
+        <div class="check-data" style="position: absolute; top: 450px">
+            <div class="alert alert-primary" role="alert"><strong>Background does not print.</strong></div>
+            <button type="button" style="float: right;" class="btn btn-primary" @click="printCheck(false)">Print (Ctrl + P)</button>
+            <button type="button" style="float: right; margin-right: 10px;" class="btn btn-outline-primary" @click="printCheck(true)">Print Blank</button>
+            <form class="row g-3">
+                <div class="col-md-4">
+                    <label class="form-label">Check Font</label>
+                    <select class="form-select" v-model="selectedFont">
+                        <option value="">Original (Default)</option>
+                        <option value="'JetBrains Mono', 'Courier New', monospace">JetBrains Mono</option>
+                        <option value="'IBM Plex Mono', 'Courier New', monospace">IBM Plex Mono</option>
+                        <option value="'Source Code Pro', 'Courier New', monospace">Source Code Pro</option>
+                        <option value="'Courier New', monospace">Classic Typewriter</option>
+                        <option value="'Noto Serif', 'Times New Roman', serif">Noto Serif</option>
+                        <option value="'Libre Baskerville', 'Times New Roman', serif">Libre Baskerville</option>
+                        <option value="'Georgia', 'Times New Roman', serif">Georgia</option>
+                        <option value="'Garamond', 'Times New Roman', serif">Garamond</option>
+                    </select>
+                </div>
+                <div class="col-md-6">
+                    <label for="inputEmail4" class="form-label">Account Holder Name</label>
+                    <input type="email" class="form-control" id="inputEmail4" v-model="check.accountHolderName">
+                </div>
+                <div class="col-md-6">
+                    <label class="form-label">Account Holder Name (Additional)</label>
+                    <input type="text" class="form-control" v-model="check.accountHolderNameSecondary">
+                </div>
+                <div class="col-md-6">
+                </div>
+                <div class="col-md-4">
+                    <label for="inputAddress" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="inputAddress" v-model="check.accountHolderAddress">
+                </div>
+                <div class="col-md-2">
+                    <label for="inputCity" class="form-label">City</label>
+                    <input type="text" class="form-control" v-model="check.accountHolderCity">
+                </div>
+                <div class="col-md-2">
+                    <label for="inputState" class="form-label">State</label>
+                    <input type="text" class="form-control" v-model="check.accountHolderState">
+                </div>
+                <div class="col-md-2">
+                    <label for="inputZip" class="form-label">Zip</label>
+                    <input type="text" class="form-control" v-model="check.accountHolderZip">
+                </div>
+            </form>
+            <form class="row g-3" style="margin-top: 30px; border-top: 1px solid #e7e7e7;">
+                <div class="col-md-2">
+                    <label for="inputEmail4" class="form-label">Check Number</label>
+                    <input type="email" class="form-control" id="inputEmail4" v-model="check.checkNumber">
+                </div>
+                <div class="col-md-4">
+                    <label for="inputAddress" class="form-label">Bank Name</label>
+                    <input type="text" class="form-control" id="inputAddress" v-model="check.bankName">
+                </div>
+                <div class="col-md-2">
+                    <label for="inputCity" class="form-label">Routing #</label>
+                    <input type="text" class="form-control" v-model="check.routingNumber">
+                </div>
+                <div class="col-md-2">
+                    <label for="inputState" class="form-label">Account #</label>
+                    <input type="text" class="form-control" v-model="check.bankAccountNumber">
+                </div>
+                <div class="col-md-6">
+                    <label for="inputZip" class="form-label">Memo</label>
+                    <input type="text" class="form-control" v-model="check.memo">
+                </div>
+            </form>
+            <form class="row g-3" style="margin-top: 30px; border-top: 1px solid #e7e7e7;">
+                <div class="col-md-2">
+                    <label for="inputEmail4" class="form-label">Amount</label>
+                    <input type="email" class="form-control" id="inputEmail4" v-model="check.amount">
+                </div>
+                <div class="col-md-6">
+                    <label for="inputZip" class="form-label">Pay To</label>
+                    <input type="text" class="form-control" v-model="check.payTo">
+                </div>
+                <div class="col-md-2">
+                    <label for="inputEmail4" class="form-label">Date</label>
+                    <input type="email" class="form-control" id="inputEmail4" v-model="check.date">
+                </div>
+                <div class="col-md-6">
+                    <label for="inputZip" class="form-label">Signature</label>
+                    <input type="text" class="form-control" v-model="check.signature">
+                </div>
+            </form>
+            <div class="col-12" style="margin-top: 30px;">
+                <button type="button" class="btn btn-primary" @click="saveToHistory">Save to History</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import print from 'print-js';
+import { ToWords } from 'to-words';
+import { ref, reactive, nextTick, watch, onMounted, onUnmounted } from 'vue'
+import { formatMoney } from '../utilities.ts'
+import { useAppStore } from '../stores/app.ts'
+
+const state = useAppStore()
+
+const toWordsTool = new ToWords({
+  localeCode: 'en-US',
+  converterOptions: {
+    currency: true,
+    ignoreDecimal: false,
+    ignoreZeroCurrency: false,
+    doNotAddOnly: true,
+  },
+});
+
+const toWords: (denom: number | string) => string = (denom) => {
+    try {
+        return toWordsTool.convert(Number(denom), );
+    } catch (e) {
+        return `${e}`;
+    }
+}
+
+const printBlank = ref(false)
+
+function resetPrintMode() {
+        printBlank.value = false
+}
+
+function printCheck (blank = false) {
+        printBlank.value = blank
+    const style = document.createElement('style');
+    style.textContent = `
+      @media print {
+        @page {
+          margin: 0;
+        }
+        body {
+          transform: scale(1);
+          transform-origin: top center;
+          width: 149%;
+          margin: 0;
+          padding: 0;
+        }
+        .wrapper > *:not(.check-box) {
+          display: none !important;
+        }
+        .check-data {
+            display: none;
+        }
+        .check-box {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          margin: 0;
+          padding: 0px;
+          background-color: white;
+          background: white !important;
+          border: none !important;
+          box-shadow: none !important;
+        }
+        .check-box-print {
+          position: relative;
+        }
+      }
+    `;
+        document.head.appendChild(style);
+        nextTick(() => {
+                window.print();
+                style.remove();
+        })
+}
+
+function saveToHistory () {
+    let checkList = JSON.parse(localStorage.getItem('checkList') || '[]')
+    checkList.push(check)
+    localStorage.setItem('checkList', JSON.stringify(checkList))
+}
+
+function genNewCheck () {
+    let checkList = JSON.parse(localStorage.getItem('checkList') || '[]')
+    let recentCheck = checkList[checkList.length - 1]
+    let check = {}
+    check.accountHolderName = recentCheck?.accountHolderName || 'John Smith'
+    check.accountHolderNameSecondary = recentCheck?.accountHolderNameSecondary || ''
+    check.accountHolderAddress = recentCheck?.accountHolderAddress || '123 Cherry Tree Lane'
+    check.accountHolderCity = recentCheck?.accountHolderCity || 'New York'
+    check.accountHolderState = recentCheck?.accountHolderState || 'NY'
+    check.accountHolderZip = recentCheck?.accountHolderZip || '10001'
+    check.checkNumber = recentCheck?.checkNumber ? (parseInt(recentCheck?.checkNumber) + 1) : '100'
+    check.date = new Date().toLocaleDateString()
+    check.bankName = recentCheck?.bankName || 'Bank Name, INC'
+    check.amount = '0.00'
+    check.payTo = 'Michael Johnson'
+    check.memo = recentCheck?.memo || 'Rent'
+    check.signature = recentCheck?.signature || 'John Smith'
+    check.routingNumber = recentCheck?.routingNumber || '022303659'
+    check.bankAccountNumber = recentCheck?.bankAccountNumber || '000000000000'
+    return check
+}
+
+const check = reactive(
+    genNewCheck()
+)
+
+const line = ref(null)
+const selectedFont = ref('')
+
+watch(check, async () => {
+    await nextTick(() => {
+        let computedLine = line?.value?.clientWidth
+        check.lineLength = computedLine
+    })
+}, { immediate: true })
+
+function handlePrintShortcut(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key === 'p') {
+        event.preventDefault();
+        printCheck();
+    }
+}
+
+onMounted(() => {
+    if (state.check) {
+        check.accountHolderName = state.check.accountHolderName
+        check.accountHolderNameSecondary = state.check.accountHolderNameSecondary
+        check.accountHolderAddress = state.check.accountHolderAddress
+        check.accountHolderCity = state.check.accountHolderCity
+        check.accountHolderState = state.check.accountHolderState
+        check.accountHolderZip = state.check.accountHolderZip
+        check.checkNumber = state.check.checkNumber
+        check.date = state.check.date
+        check.bankName = state.check.bankName
+        check.amount = state.check.amount
+        check.payTo = state.check.payTo
+        check.memo = state.check.memo
+        check.signature = state.check.signature
+        check.routingNumber = state.check.routingNumber
+        check.bankAccountNumber = state.check.bankAccountNumber
+    }
+    state.check = null
+
+    window.addEventListener('keydown', handlePrintShortcut);
+    window.addEventListener('afterprint', resetPrintMode);
+});
+
+onUnmounted(() => {
+    window.removeEventListener('keydown', handlePrintShortcut);
+    window.removeEventListener('afterprint', resetPrintMode);
+});
+
+</script>
+
+<style>
+
+label {
+    font-weight: bold;
+}
+.memo-data {
+    font-family: Caveat;
+    font-size: 30px;
+    max-width: 350px;
+    line-height: 0.65;
+}
+.signature-data {
+    font-family: Caveat;
+    font-size: 40px;
+    transform: rotate(-2deg);
+}
+.amount-line-data {
+    text-transform: capitalize;
+}
+.date-data, .pay-to-data, .amount-data{
+    font-size: 20px;
+    font-weight: bold;
+}
+.check-data {
+    margin-top: 50px;
+    padding: 50px 120px;
+    border-top: 1px solid #e6e6e6;
+}
+.bank-name{
+    font-size: 20px;
+    font-weight: bold;
+}
+.account-holder-name {
+    font-size: 20px;
+    font-weight: bold;
+}
+.check-number-human {
+    font-size: 20px;
+    font-weight: bold;
+}
+.amount-box::before {
+    content: "$";
+    font-size: 20px;
+    margin-left: -15px;
+    font-weight: bold;
+}
+.amount-box {
+    width: 225px;
+    height: 40px;
+    border: 1px solid #c7c7c7;
+    background-color: white;
+}
+.check-box {
+    width: 1200px;
+    height: 1553px;
+    border: 1px solid #e6e6e6;
+    background-color: white;
+    margin: 0 auto;
+    background: url('../assets/checkbg.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+}
+
+#check-box {
+    width: 100%;
+}
+
+@font-face {
+    font-family: 'banking';
+    src: url('../assets/micrenc.ttf');
+}
+
+
+.banking {
+    font-family: 'banking';
+    font-size: 37px;
+}
+.check-box .banking {
+    font-family: 'banking';
+}
+.dollar-line::after{
+    content: "Dollars";
+    font-size: 18px;
+    position: absolute;
+    right: -73px;
+    top: 0;
+}
+.dollar-line {
+    width: 840px;
+    display: block;
+    border-bottom: 1px solid black;
+    margin-left: 10px;
+    margin-top: 20px;
+}
+.payto-line {
+    width: 776px;
+    display: block;
+    border-bottom: 1px solid black;
+    margin-left: 73px;
+    border-right: 1px solid black;
+    height: 28px;
+    margin-top: -32px;
+}
+</style>
---
+++ micrenc.ttf
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+
+    <div class="container">
+        <div style="padding-bottom: 20px; padding-top: 20px;">
+            <img src="@/assets/pmc.png"  />
+        </div>
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <RouterLink to="/" class="nav-link" :class="{'active': $route.path == '/'}">Check</RouterLink>
+            </li>
+            <li class="nav-item">
+                <RouterLink to="/history" class="nav-link" :class="{'active': $route.path == '/history'}">History</RouterLink>
+            </li>
+        </ul>
+      <nav>
+      </nav>
+    </div>
+
+    <div class="container">
+      <RouterView />
+    </div>
+</template>
+
+<style scoped>
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+</style>
